dc475ad5dca9d58680ee5f1825a0d35a4386be8c
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-13 14:23:37 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,8 +1,8 @@
 def simple_curfew_checker(time)
  if time = 11
-   return "You're in trouble! Better get home!"
+   return "You're in trouble! Better get home quick!"
   if time >= 11
-   return "You're in trouble! Better get home!"
+   return "You're in trouble! Better get home quick!"
 end
 end
 
